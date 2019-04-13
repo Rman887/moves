b3993183de9122607402a26a58f97401f5642fcb
urolyi1 <urolyi1@gmail.com>
2019-04-13 17:57:19 -0400

    Add login page

---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <label><b>Username</b></label>
+    <input type="text" name="uname" v-model="usernameText">
+
+    <label><b>Password</b></label>
+    <input type="password" name="psw" v-model="passwordText">
+
+    <h1> {{usernameText}} </h1>
+    <h1> {{passwordText}} </h1>
+
+    <button v-on:click="submitLogin">Login</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+  components: {
+  },
+
+  data () {
+    return {
+      usernameText: '',
+      passwordText: ''
+    }
+  },
+
+  methods: {
+    submitLogin: function (event) {
+      console.log('SUBMITTED!')
+    }
+  }
+}
+</script>
