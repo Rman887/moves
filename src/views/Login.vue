b1e58f3e671abc9dbf5fe981ec38049db75276fe
urolyi1 <urolyi1@gmail.com>
2019-04-13 18:42:41 -0400

    Add registration page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,9 @@
 <template>
   <div>
-    <h1>Login</h1>
-    <label><b>Username</b></label>
-    <input type="text" name="uname" v-model="usernameText">
-
-    <label><b>Password</b></label>
-    <input type="password" name="psw" v-model="passwordText">
-
-    <h1> {{usernameText}} </h1>
-    <h1> {{passwordText}} </h1>
+    <h1><b>Login</b></h1>
+    <input type="text" placeholder="Username" name="uname" v-model="usernameText">
+    <br>
+    <input type="password" placeholder="Password" name="psw" v-model="passwordText">
 
     <button v-on:click="submitLogin">Login</button>
   </div>
@@ -35,3 +30,9 @@ export default {
   }
 }
 </script>
+
+<style>
+  button{
+
+  }
+</style>
---
+++ src/views/Registration.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1><b>Register</b></h1>
+
+    <input type="text" placeholder="Username" name="uname" v-model="usernameText">
+    <br>
+    <input type="text" placeholder="Email" name="email" v-model="emailText">
+    <br>
+    <input type="password" placeholder="Password" name="psw" v-model="passwordText">
+    <br>
+    <input type="password" placeholder="Confirm Password" name="confpsw" v-model="confPasswordText">
+    <br>
+    <button v-on:click="submitRegistration">Register</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'registration',
+  components: {
+  },
+
+  data () {
+    return {
+      usernameText: '',
+      emailText: '',
+      passwordText: '',
+      confPasswordText: ''
+    }
+  },
+
+  methods: {
+    submitRegistration: function (event) {
+      console.log('REGISTERING!')
+    }
+  }
+}
+</script>
