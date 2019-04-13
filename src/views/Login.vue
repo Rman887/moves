45d84b1e21bde3dc469a2145c564e03b812bd11e
urolyi1 <urolyi1@gmail.com>
2019-04-13 19:03:57 -0400

    Add style to login and registration

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <h1><b>Login</b></h1>
-    <input type="text" placeholder="Username" name="uname" v-model="usernameText">
-    <br>
-    <input type="password" placeholder="Password" name="psw" v-model="passwordText">
-
-    <button v-on:click="submitLogin">Login</button>
+    <form v-on:submit.prevent="submitLogin">
+      <input type="text" placeholder="Username" name="uname" v-model="usernameText">
+      <br>
+      <input type="password" placeholder="Password" name="psw" v-model="passwordText">
+      <br>
+      <input type="submit" text="Login">
+    </form>
   </div>
 </template>
 
@@ -30,9 +32,35 @@ export default {
   }
 }
 </script>
-
 <style>
-  button{
+  h1 {
+    margin-bottom: 20px;
+  }
 
+  input[type='text'], input[type='password'] {
+    padding: 10px;
+  }
+
+  input {
+    margin-bottom: 15px;
+    border-radius: 5px;
+    border: 1px solid grey;
+    outline: none;
+    width: 250px;
+    height: 45px;
+  }
+
+  input[type='submit'] {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    cursor: pointer;
+    transition: 0.2s ease;
+    background-color: grey;
+    color: white;
+  }
+
+  input[type='submit']:hover {
+    background-color: red;
   }
 </style>
--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
     <h1><b>Register</b></h1>
-
-    <input type="text" placeholder="Username" name="uname" v-model="usernameText">
-    <br>
-    <input type="text" placeholder="Email" name="email" v-model="emailText">
-    <br>
-    <input type="password" placeholder="Password" name="psw" v-model="passwordText">
-    <br>
-    <input type="password" placeholder="Confirm Password" name="confpsw" v-model="confPasswordText">
-    <br>
-    <button v-on:click="submitRegistration">Register</button>
+    <form v-on:submit.prevent="submitRegistration">
+      <input type="text" placeholder="Username" name="uname" v-model="usernameText">
+      <br>
+      <input type="text" placeholder="Email" name="email" v-model="emailText">
+      <br>
+      <input type="password" placeholder="Password" name="psw" v-model="passwordText">
+      <br>
+      <input type="password" placeholder="Confirm Password" name="confpsw" v-model="confPasswordText">
+      <br>
+      <input type="submit" text="Register">
+    </form>
   </div>
 </template>
 
@@ -37,3 +38,35 @@ export default {
   }
 }
 </script>
+<style>
+  h1 {
+    margin-bottom: 20px;
+  }
+
+  input[type='text'], input[type='password'] {
+    padding: 10px;
+  }
+
+  input {
+    margin-bottom: 15px;
+    border-radius: 5px;
+    border: 1px solid grey;
+    outline: none;
+    width: 250px;
+    height: 45px;
+  }
+
+  input[type='submit'] {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    cursor: pointer;
+    transition: 0.2s ease;
+    background-color: grey;
+    color: white;
+  }
+
+  input[type='submit']:hover {
+    background-color: red;
+  }
+</style>
